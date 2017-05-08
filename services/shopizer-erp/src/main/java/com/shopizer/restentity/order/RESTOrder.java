bbf5dc36a7ff85eb0e608a0a7e42b83795301590
Samson <c.samson@cgi.com>
2017-05-08 12:27:02 -0400

    Order

--- services/shopizer-erp/src/main/java/com/shopizer/restentity/order/RESTOrder.java
+++ services/shopizer-erp/src/main/java/com/shopizer/restentity/order/RESTOrder.java
@@ -1,7 +1,81 @@
 package com.shopizer.restentity.order;
 
+import java.util.List;
+
 import com.shopizer.restentity.common.RESTEntity;
+import com.shopizer.restentity.customer.RESTCustomer;
 
 public class RESTOrder extends RESTEntity {
+	
+	private Long number;
+	
+	private String estimated;
+	
+	private String created;
+	
+
+	private RESTCustomer customer;
+	
+	private List<RESTOrderTotal> orderTotals;
+	
+	private String total;
+	
+	private String description;
+
+	public Long getNumber() {
+		return number;
+	}
+
+	public void setNumber(Long number) {
+		this.number = number;
+	}
+
+	public String getEstimated() {
+		return estimated;
+	}
+
+	public void setEstimated(String estimated) {
+		this.estimated = estimated;
+	}
+
+	public String getCreated() {
+		return created;
+	}
+
+	public void setCreated(String created) {
+		this.created = created;
+	}
+
+	public RESTCustomer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(RESTCustomer customer) {
+		this.customer = customer;
+	}
+
+	public List<RESTOrderTotal> getOrderTotals() {
+		return orderTotals;
+	}
+
+	public void setOrderTotals(List<RESTOrderTotal> orderTotals) {
+		this.orderTotals = orderTotals;
+	}
+
+	public String getTotal() {
+		return total;
+	}
+
+	public void setTotal(String total) {
+		this.total = total;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 }
---
+++ services/shopizer-erp/src/main/java/com/shopizer/restentity/order/RESTOrderTotal.java
@@ -0,0 +1,28 @@
+package com.shopizer.restentity.order;
+
+public class RESTOrderTotal {
+	
+	private String value;
+	private String name;
+	private int order;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public int getOrder() {
+		return order;
+	}
+	public void setOrder(int order) {
+		this.order = order;
+	}
+
+}
